e5bde7479d4d5b478a19d9bcf7e6294640a8619a
Suyash Verma <sverma29@gmail.com>
2025-11-10 04:39:09 -0500

    practice book system

--- 09 bookSystem/book_system.js
+++ 09 bookSystem/book_system.js
@@ -26,7 +26,8 @@ function showbooks() {
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
         <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        <button onclick="editbook(${index})">Edit</button>`
+        <button onclick="editbook(${index})">Edit</button>
+        <button onclick="deletebook(${index})">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -40,6 +41,12 @@ function editbook(index) {
     books.splice(index, 1); // Remove old entry
     showbooks(); // Refresh list
   }
+
+function deletebook(index) {
+    const book = books[index];
+    books.splice(index, 1); // Remove old entry
+    showbooks(); // Refresh list
+}
 
  function clearInputs() {
             document.getElementById('bookName').value = '';
